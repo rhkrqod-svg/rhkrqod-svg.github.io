d9d4b8485b0ea7be391afd2c03c35ff014bb6baa
rhkrqod-svg <rhkrqod-svg@users.noreply.github.com>
2026-06-29 00:40:28 +0900

    Add mobile home screen app install support

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "subway-villain-hunter-v1";
+const CORE_ASSETS = [
+  "/",
+  "/manifest.webmanifest",
+  "/assets/images/start-screen.png",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png",
+  "/assets/icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("/")))
+  );
+});
